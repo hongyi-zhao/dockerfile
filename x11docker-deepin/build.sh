d03b8a536543c14ffc39bf5a725f9daab3a6a4c0
Hongyi Zhao <hongyi.zhao@gmail.com>
2020-12-04 14:14:22 +0800

    modified:   x11docker-deepin-wine/build.sh modified:   x11docker-deepin-wine/hooks/environment modified:   x11docker-deepin/build.sh modified:   x11docker-deepin/hooks/environment

--- x11docker-deepin/build.sh
+++ x11docker-deepin/build.sh
@@ -8,13 +8,13 @@ pwd -P
 
 
 #The docker images can be created on locale host with:
-#git clone https://github.com/hongyi-zhao/dockerfile-x11docker-deepin.git dockerfile-x11docker-deepin.git
-#cd dockerfile-x11docker-deepin.git
+#git clone https://github.com/hongyi-zhao/dockerfile.git dockerfile.git
+#cd dockerfile.git/x11docker-deepin.git
 . $topdir/hooks/environment
 docker build --network host --build-arg http_proxy='' --build-arg https_proxy='' \
              --build-arg DEEPIN_MIRROR=${DEEPIN_MIRROR} \
              --build-arg DEEPIN_APPSTORE_MIRROR=${DEEPIN_APPSTORE_MIRROR} \
              --build-arg DOCKER_TAG=${DOCKER_TAG} \
              --build-arg DEEPIN_RELEASE=${DEEPIN_RELEASE} \
-             -t ${DOCKER_REPO}:${DOCKER_TAG} .
+             -t hongyizhao/deepin:${DOCKER_TAG} .
 
