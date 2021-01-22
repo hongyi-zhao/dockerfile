8d1f160d04aea4dadd45adecef2956e98809321b
Hongyi Zhao <hongyi.zhao@gmail.com>
2021-01-22 11:20:22 +0800

    modified:   x11docker-deepin/build.sh modified:   x11docker-deepin/hooks/build modified:   x11docker-deepin/hooks/push

--- x11docker-deepin/build.sh
+++ x11docker-deepin/build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-script_realpath="$(realpath -e -- "${BASH_SOURCE[0]}")"
 
-topdir=$(
-cd -P -- "$(dirname -- "$script_realpath")" &&
-pwd -P
-) 
-
+script_name_sh=$HOME/.local/libexec/script_name.sh
+source $script_name_sh ${BASH_SOURCE[0]}
 
 #The docker images can be created on locale host with:
 #git clone https://github.com/hongyi-zhao/dockerfile.git dockerfile.git
 #cd dockerfile.git/x11docker-deepin.git
-. $topdir/hooks/environment
+. $script_realdirname/hooks/environment
 docker build --network host --build-arg http_proxy='' --build-arg https_proxy='' \
              --build-arg DEEPIN_MIRROR=${DEEPIN_MIRROR} \
              --build-arg DEEPIN_APPSTORE_MIRROR=${DEEPIN_APPSTORE_MIRROR} \
