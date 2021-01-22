f072ed5746481c2d5eb89d4c984c0f19b4140987
Hongyi Zhao <hongyi.zhao@gmail.com>
2021-01-22 15:34:09 +0800

    modified:   x11docker-deepin-wine/build.sh modified:   x11docker-deepin-wine/hooks/build modified:   x11docker-deepin-wine/hooks/environment modified:   x11docker-deepin-wine/hooks/push modified:   x11docker-deepin/build.sh modified:   x11docker-deepin/hooks/environment

--- x11docker-deepin-wine/build.sh
+++ x11docker-deepin-wine/build.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
+
 script_realpath="$(realpath -e -- "${BASH_SOURCE[0]}")"
-
-topdir=$(
-cd -P -- "$(dirname -- "$script_realpath")" &&
+script_dirname=$(
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" &&
 pwd -P
-) 
+)
+script_name=$(basename -- "${BASH_SOURCE[0]}")
 
+pkg_repo=${script_realpath%.*}
+script_realdirname=${script_realpath%/*}
+script_realname=${script_realpath##*/}
+script_realbasename=${script_realname%.*}
+script_realextname=${script_realname##*.}
+
+script_path=$script_dirname/$script_name
+script_basename=${script_name%.*}
+script_extname=${script_name##*.}
 
 #The docker images can be created on locale host with:
 #git clone https://github.com/hongyi-zhao/dockerfile.git dockerfile.git
 #cd dockerfile.git/x11docker-deepin-wine.git
-. $topdir/hooks/environment
+. $script_realdirname/hooks/environment
 docker build --network host --build-arg http_proxy='' --build-arg https_proxy='' \
              --build-arg DEEPIN_MIRROR=${DEEPIN_MIRROR} \
              --build-arg DEEPIN_APPSTORE_MIRROR=${DEEPIN_APPSTORE_MIRROR} \
--- x11docker-deepin/build.sh
+++ x11docker-deepin/build.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-script_name_sh=$HOME/.local/libexec/script_name.sh
-source $script_name_sh ${BASH_SOURCE[0]}
+script_realpath="$(realpath -e -- "${BASH_SOURCE[0]}")"
+script_dirname=$(
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" &&
+pwd -P
+)
+script_name=$(basename -- "${BASH_SOURCE[0]}")
+
+pkg_repo=${script_realpath%.*}
+script_realdirname=${script_realpath%/*}
+script_realname=${script_realpath##*/}
+script_realbasename=${script_realname%.*}
+script_realextname=${script_realname##*.}
+
+script_path=$script_dirname/$script_name
+script_basename=${script_name%.*}
+script_extname=${script_name##*.}
 
 #The docker images can be created on locale host with:
 #git clone https://github.com/hongyi-zhao/dockerfile.git dockerfile.git
