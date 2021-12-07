b322e6913347dd8ce0eb663eee52d99d1c7678b5
Hongyi Zhao <hongyi.zhao@gmail.com>
2021-12-07 09:20:52 +0800

    modified:   deepin/build.sh modified:   deepin/wine/Dockerfile

--- deepin/build.sh
+++ deepin/build.sh
@@ -10,4 +10,5 @@ docker build --network host --build-arg http_proxy='' --build-arg https_proxy=''
 docker build --network host --build-arg http_proxy='' --build-arg https_proxy='' \
              --build-arg DEEPIN_MIRROR=${DEEPIN_MIRROR} \
              --build-arg DEEPIN_RELEASE=${DEEPIN_RELEASE} \
+             --build-arg DEEPIN_BASE_IMAGE=hongyizhao/deepin:${DEEPIN_RELEASE} \
              -t hongyizhao/deepin-wine:${DEEPIN_RELEASE} -f wine/Dockerfile .
