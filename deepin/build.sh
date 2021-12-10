d2a7fed297f6b3e808b4406cd6e84bac119c2116
Hongyi Zhao <hongyi.zhao@gmail.com>
2021-12-10 15:23:01 +0800

    modified:   deepin/base/Dockerfile modified:   deepin/build.sh

--- deepin/build.sh
+++ deepin/build.sh
@@ -5,7 +5,7 @@ docker build --network host --build-arg http_proxy='' --build-arg https_proxy=''
              --build-arg OPENSOURCE_MIRROR=${OPENSOURCE_MIRROR} \
              --build-arg DEEPIN_REPOSITORY=${DEEPIN_REPOSITORY} \
              --build-arg DEEPIN_RELEASE=${DEEPIN_RELEASE} \
-             -t hongyizhao/deepin:${DEEPIN_RELEASE} -f base/Dockerfile .
+             -t hongyizhao/deepin-desktop:${DEEPIN_RELEASE} -f base/Dockerfile .
 
 docker build --network host --build-arg http_proxy='' --build-arg https_proxy='' \
              --build-arg OPENSOURCE_MIRROR=${OPENSOURCE_MIRROR} \
@@ -15,5 +15,5 @@ docker build --network host --build-arg http_proxy='' --build-arg https_proxy=''
              --build-arg DEEPIN_RELEASE=${DEEPIN_RELEASE} \
              --build-arg DEEPIN_PRINTER_RELEASE=${DEEPIN_PRINTER_RELEASE} \
              --build-arg DEEPIN_APPSTORE_RELEASE=${DEEPIN_APPSTORE_RELEASE} \
-             --build-arg DEEPIN_BASE_IMAGE=hongyizhao/deepin:${DEEPIN_RELEASE} \
+             --build-arg DEEPIN_BASE_IMAGE=hongyizhao/deepin-desktop:${DEEPIN_RELEASE} \
              -t hongyizhao/deepin-wine:${DEEPIN_RELEASE} -f wine/Dockerfile .
